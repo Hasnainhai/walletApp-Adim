0a64fee6761168102022d45061dd85936dfccfa5
Basit ali <basitalyshah51214@gmail.com>
2024-05-23 12:06:20 -0700

    change to add subscription pop up

--- lib/view/my_subscribtions.dart
+++ lib/view/my_subscribtions.dart
@@ -6,6 +6,7 @@ import 'package:wallet_admin/res/components/vertical_spacing.dart';
 import 'package:wallet_admin/res/keys.dart';
 import 'package:wallet_admin/res/responsive.dart';
 import 'package:wallet_admin/view/slide_menu.dart';
+import 'package:wallet_admin/view/widgets/add_field.dart';
 
 import '../res/components/roundedButton.dart';
 
@@ -51,7 +52,10 @@ class MySubscribtions extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
                               RoundedButton(
-                                  ontap: () {}, title: 'Add Subscribtions')
+                                  ontap: () {
+                                    showCustomDialog(context);
+                                  },
+                                  title: 'Add Subspcribtions')
                             ],
                           ),
                           const VerticalSpeacing(30.0),
@@ -232,6 +236,137 @@ class MySubscribtions extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  void showCustomDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: const Color(0xffF8F8F8),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: SizedBox(
+            width: 446.77,
+            height: MediaQuery.of(context).size.height / 1.5,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      'Subscriptions details',
+                      style: GoogleFonts.getFont(
+                        "Poppins",
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: AppColor.textColor1,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const AddField(
+                    title: "Duration of subscriptions",
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const AddField(
+                    title: "Subscriptions Charges",
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Date",
+                        style: GoogleFonts.getFont(
+                          "Poppins",
+                          textStyle: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: AppColor.textColor1,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 38,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          border: Border.all(
+                            color: Colors
+                                .grey, // Replace with your AppColor.borderColor
+                          ),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8.0,
+                            right: 8,
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.calendar_today_outlined,
+                                    color: AppColor.textColor1,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // const AddField(
+                  //   title: "Account Number",
+                  // ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Container(
+                      height: 38,
+                      width: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: AppColor.primaryColor),
+                      child: Center(
+                        child: Text(
+                          'Add Subscriptions',
+                          style: GoogleFonts.getFont(
+                            "Poppins",
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
